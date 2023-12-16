85688bb8c4372a6f3f9e8d9bb9b9ca9d86ff9055
abujaforhadi <abujaforhadi@gmail.com>
2023-12-16 21:28:51 +0600

    1st commit

---
+++ app/src/main/java/com/example/contactapp/Constants.java
@@ -0,0 +1,35 @@
+package com.example.contactapp;
+
+public class Constants {
+
+    // database or db name
+    public static final String DATABASE_NAME = "CONTACT_DB";
+    //database version
+    public static final int DATABASE_VERSION = 1;
+
+    // table name
+    public static final String TABLE_NAME = "CONTACT_TABLE";
+
+    // table column or field name
+    public static final String C_ID = "ID";
+    public static final String C_IMAGE = "IMAGE";
+    public static final String C_NAME = "NAME";
+    public static final String C_PHONE = "PHONE";
+    public static final String C_EMAIL = "EMAIL";
+    public static final String C_NOTE = "NOTE";
+
+    // query for create table
+    public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "( "
+            + C_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + C_IMAGE + " TEXT, "
+            + C_NAME + " TEXT, "
+            + C_PHONE + " TEXT, "
+            + C_EMAIL + " TEXT, "
+            + C_NOTE + " TEXT, "
+            + " );";
+
+
+    // Create database helper class for CRUD Query And Database Creation
+
+
+}
---
+++ app/src/main/java/com/example/contactapp/ContactDetails.java
@@ -0,0 +1,95 @@
+package com.example.contactapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.Format;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class ContactDetails extends AppCompatActivity {
+
+    //view
+    private TextView nameTv,phoneTv,emailTv,addedTimeTv,updatedTimeTv,noteTv;
+    private ImageView profileIv;
+
+    private String id;
+
+    //database helper
+    private DbHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_details);
+
+        //init db
+        dbHelper = new DbHelper(this);
+
+        //get data from intent
+        Intent intent = getIntent();
+        id = intent.getStringExtra("contactId");
+
+        //init view
+        nameTv = findViewById(R.id.nameTv);
+        phoneTv = findViewById(R.id.phoneTv);
+        emailTv = findViewById(R.id.emailTv);
+        noteTv = findViewById(R.id.noteTv);
+
+        profileIv = findViewById(R.id.profileIv);
+
+        loadDataById();
+
+    }
+
+    private void loadDataById() {
+
+        //get data from database
+        //query for find data by id
+        String selectQuery =  "SELECT * FROM "+Constants.TABLE_NAME + " WHERE " + Constants.C_ID + " =\"" + id + "\"";
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery,null);
+
+        if (cursor.moveToFirst()){
+            do {
+                //get data
+                String name =  ""+cursor.getString(cursor.getColumnIndexOrThrow(Constants.C_NAME));
+                String image = ""+cursor.getString(cursor.getColumnIndexOrThrow(Constants.C_IMAGE));
+                String phone = ""+cursor.getString(cursor.getColumnIndexOrThrow(Constants.C_PHONE));
+                String email = ""+cursor.getString(cursor.getColumnIndexOrThrow(Constants.C_EMAIL));
+                String note = ""+cursor.getString(cursor.getColumnIndexOrThrow(Constants.C_NOTE));
+
+
+
+
+                //set data
+                nameTv.setText(name);
+                phoneTv.setText(phone);
+                emailTv.setText(email);
+                noteTv.setText(note);
+
+
+                if (image.equals("null")){
+                    profileIv.setImageResource(R.drawable.ic_baseline_person_24);
+                }else {
+                    profileIv.setImageURI(Uri.parse(image));
+                }
+
+            }while (cursor.moveToNext());
+        }
+
+        db.close();
+
+    }
+
+
+}
